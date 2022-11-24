d7ccc68df85030e381d8e0cc4af7498b6551f89b
RickCarr <rick.carr@telus.com>
2022-11-23 20:30:38 -0800

    tested 5 scenarios, passed all with wordsearch. pair programming with jhon u.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,41 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  if (word.length === 0) return 'Enter a word to search.';
+  if (letters.length === 0) return 'This Array is Empty!';
+    
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  console.log(horizontalJoin);
+  //horizontal
+    
+  for (const line of horizontalJoin) {
+    console.log(line);
+    if (line.includes(word)) return true;
+  }
+
+  // vertical
+  const transposedArray = [];
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      transposedArray.push([]);
+    }
+    break;
+  }
+
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      transposedArray[j].push(letters[i][j]);
+    }
+  }
+
+  const verticalJoin = transposedArray.map(ls => ls.join(''));
+  console.log(transposedArray);
+
+  for (const line of verticalJoin) {
+    console.log(line);
+    if (line.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
